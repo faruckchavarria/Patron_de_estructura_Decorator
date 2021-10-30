fd0969a50c860a5bc184986264cc22e79b17789e
faruckchavarria <evafaruck@gmail.com>
2021-10-30 15:58:03 -0600

    Se crea la clase llamada cuenta plaza

---
+++ src/main/java/org/faruck/CuentaPlazo.java
@@ -0,0 +1,27 @@
+package org.faruck;
+
+public class CuentaPlazo extends Decorador {
+    //interés aplicado a la cuenta a plazo
+    private double interes;
+    //plazo en años
+    private int plazo;
+    public CuentaPlazo(Cuenta c, double interes, int plazo) {
+        super(c);
+        this.interes = interes;
+        this.plazo = plazo;
+    }
+    public void aplicaIntereses() {
+        double saldoActual = super.saldo();
+        super.getCuenta().ingresar(saldoActual * (interes / 100) * plazo);
+    }
+    public double getInteres() {
+        return this.interes;
+    }
+    public int getPlazo() {
+        return this.plazo;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + "\nInterés: " + interes + " - Plazo: " + plazo;
+    }
+}
